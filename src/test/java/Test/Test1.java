0fc3fe7b38608d632ec5e9cde1027408dc559eb6
Deniz <sn.denizz@gmail.com>
2022-11-25 23:52:09 -0500

    New initial commit

--- src/test/java/Test/Test1.java
+++ src/test/java/Test/Test1.java
@@ -4,12 +4,7 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("how are you?");
-
-        // work 1
-
-        // work 2
-
-        //work 3
+        System.out.println("GitHub Repo ready to published");
 
 
     }
